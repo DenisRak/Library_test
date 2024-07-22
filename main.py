738183fbe0d9d2bd5f761b46e1f71ef8b2fee4f2
Rak Denis <rakdenis32@gmail.com>
2024-07-22 16:10:23 +0300

    update main in main

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ def main():
     IN_OR_OUT = {"+": "в наличии", "-": "выдана"}
 
     while True:
-        print("\nМеню библиотеки:")
+        print("\nВы находитесь в главном меню библиотеки:")
         print("1. Добавить книгу")
         print("2. Удалить книгу")
         print("3. Искать книгу")
@@ -107,7 +107,8 @@ def main():
 
         if choice == '1':
             while True:
-                print("1. Добавляем книгу")
+                print("Вы находитесь в меню добавления книг")
+                print("1. Продолжаем добавлять книгу")
                 print("0. Закончили добавлять")
                 subchoice = input("Выберите действие: ")
                 if subchoice == "1":
@@ -129,12 +130,22 @@ def main():
             except ValueError:
                 print("ID должен быть числом")
         elif choice == '3':
-            criterion = input("Искать по (id/title/author/year): ").strip().lower()
-            if criterion in ('id', 'title', 'author', 'year'):
-                query = input("Введите поисковый запрос: ").strip().lower()
-                library.search_books(criterion, query)
-            else:
-                print('Нет такого критерия. Выберите из id title author year')
+            while True:
+                print("Вы находитесь в меню поиска книг")
+                print("1. Продолжаем искать книгу")
+                print("0. Закончили искать")
+                subchoice = input("Выберите действие: ")
+                if subchoice == "1":
+                    criterion = input("Искать по (id/title/author/year): ").strip().lower()
+                    if criterion in ('id', 'title', 'author', 'year'):
+                        query = input("Введите поисковый запрос: ").strip().lower()
+                        library.search_books(criterion, query)
+                    else:
+                        print('Нет такого критерия. Выберите из id title author year')
+                elif subchoice == "0":
+                    break
+                else:
+                    print("Некорректный выбор. Попробуйте снова.")
         elif choice == '4':
             library.display_books()
         elif choice == '5':
